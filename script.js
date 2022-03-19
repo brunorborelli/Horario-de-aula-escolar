5a57df02ea02ecd89d871b4d43821741e52f0d50
BrunoBorelli <rodriguesborellibruno@gmail.com>
2022-03-19 11:56:09 -0300

    Layout change; JS file added with button codes

---
+++ script.js
@@ -0,0 +1,24 @@
+const adicionar = document.getElementById('adicionar-button');
+
+//variaveis de professores
+
+//segunda feira
+let segunda1 = document.getElementById('segunda1').value;
+let segunda2 = document.getElementById('segunda2').value;
+let segunda3 = document.getElementById('segunda3').value;
+let segunda4 = document.getElementById('segunda4').value;
+let segunda5 = document.getElementById('segunda5').value;
+let segunda6 = document.getElementById('segunda6').value;
+let segunda7 = document.getElementById('segunda7').value;
+
+if(segunda1 == segunda2){
+    
+   
+
+}
+    
+    
+
+
+
+adicionar.addEventListener('click', alert1); 
