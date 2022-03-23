ae88c0f550b07a6619f114a0f53f90562387a3fa
BrunoBorelli <rodriguesborellibruno@gmail.com>
2022-03-23 00:26:52 -0300

    Added #ID to all itens from table2

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 const adicionar = document.getElementById('adicionar-button');
 const remover = document.getElementById('remover-button');
 
-//variaveis de professores
+//variaveis de professores (tabela de restrições)
 
-//segunda feira
+
+
+
+
+//segunda feira - Class Schedule
 let segunda1 = document.getElementById('segunda1').value;
 let segunda2 = document.getElementById('segunda2').value;
 let segunda3 = document.getElementById('segunda3').value;
@@ -12,13 +16,54 @@ let segunda5 = document.getElementById('segunda5').value;
 let segunda6 = document.getElementById('segunda6').value;
 let segunda7 = document.getElementById('segunda7').value;
 
-    
+//terca feira - Class Schedule
+let terca1 = document.getElementById('segunda1').value;
+let terca2 = document.getElementById('segunda2').value;
+let terca3 = document.getElementById('segunda3').value;
+let terca4 = document.getElementById('segunda4').value;
+let terca5 = document.getElementById('segunda5').value;
+let terca6 = document.getElementById('segunda6').value;
+let terca7 = document.getElementById('segunda7').value;
+
+//quarta feira - Class Schedule
+let quarta1 = document.getElementById('segunda1').value;
+let quarta2 = document.getElementById('segunda2').value;
+let quarta3 = document.getElementById('segunda3').value;
+let quarta4 = document.getElementById('segunda4').value;
+let quarta5 = document.getElementById('segunda5').value;
+let quarta6 = document.getElementById('segunda6').value;
+let quarta7 = document.getElementById('segunda7').value;
+
+//quinta feira - Class Schedule
+let quinta1 = document.getElementById('segunda1').value;
+let quinta2 = document.getElementById('segunda2').value;
+let quinta3 = document.getElementById('segunda3').value;
+let quinta4 = document.getElementById('segunda4').value;
+let quinta5 = document.getElementById('segunda5').value;
+let quinta6 = document.getElementById('segunda6').value;
+let quinta7 = document.getElementById('segunda7').value;
+
+//sexta feira - Class Schedule
+let sexta1 = document.getElementById('segunda1').value;
+let sexta2 = document.getElementById('segunda2').value;
+let sexta3 = document.getElementById('segunda3').value;
+let sexta4 = document.getElementById('segunda4').value;
+let sexta5 = document.getElementById('segunda5').value;
+let sexta6 = document.getElementById('segunda6').value;
+let sexta7 = document.getElementById('segunda7').value;
+
+
+
+
+
+
     
 function display_hidden_fields()
 {
     
     
-    document.getElementById("tabela-horarios-hiden").style.display = 'table-row';   
+    document.getElementById("tabela-horarios-hiden").style.display = 'table-row';  
+
    
     
 }
